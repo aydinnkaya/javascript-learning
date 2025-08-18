2e372d0d68de6a318723c27b83c93ef006a37975
Aydın Kaya <Aydinnkaya1@gmail.com>
2025-08-18 18:18:54 +0300

    loops

--- week-1/4-alert/app.js
+++ week-1/4-alert/app.js
@@ -11,18 +11,18 @@
 
 //Prompt(); Kullanıcıdan değer alabiliyoruz...
 
-//Kullanıcıdan alınan değerler her zaman string döner.
-// let isim = prompt("İsminizi Giriniz :");
-// let yas  = prompt("Yaşınızı giriniz : ");
+// Kullanıcıdan alınan değerler her zaman string döner.
+let isim = prompt("İsminizi Giriniz :");
+let yas = prompt("Yaşınızı giriniz : ");
 
-//TÜR DÖNÜŞÜMLERİ adındaki konu ile.
+// TÜR DÖNÜŞÜMLERİ adındaki konu ile.
 
-// console.log("İsminiz : " + isim);
-// console.log("Yaşınız : "  + yas);
+console.log("İsminiz : " + isim);
+console.log("Yaşınız : " + yas);
 
-// console.log(typeof isim);
-// console.log(typeof yas);
+console.log(typeof isim);
+console.log(typeof yas);
 
 // swift de açılan alert. Yes or exit button
-// let sonuc =confirm("Silmek istediğinize emin misiniz ?");
-// console.log(sonuc);
+let sonuc = confirm("Silmek istediğinize emin misiniz ?");
+console.log(sonuc);
