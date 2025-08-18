87cce13b52b563eb69ebc78215f276e940de09b2
Aydın Kaya <Aydinnkaya1@gmail.com>
2025-08-18 16:40:09 +0300

    first commit

---
+++ week-1/6-switch-case/app.js
@@ -0,0 +1,22 @@
+let number = prompt("Lütffen 1 ie 5 arasında bir sayi giriniz");
+
+switch (number) {
+  case "1":
+    console.log("1 Numaralı sayiyi girdiniz");
+    break;
+  case "2":
+    console.log("2 Numaralı sayiyi girdiniz");
+    break;
+  case "3":
+    console.log("3 Numaralı sayiyi girdiniz");
+    break;
+  case "4":
+    console.log("4 Numaralı sayiyi girdiniz");
+    break;
+  case "5":
+    console.log("5 Numaralı sayiyi girdiniz");
+    break;
+  default:
+    console.log("girilen sayi 1 ile 5 arasinda olmalidir");
+    break;
+}
